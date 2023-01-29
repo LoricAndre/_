597fdfa3c4483a025f94ac101b08a961d5b2c6b5
LoricAndre <loric.andre@pm.me>
2023-01-29 18:47:32 +0100

    interactive bootstrap

--- .config/yadm/bootstrap.d/55-ssh-key.sh
+++ .config/yadm/bootstrap.d/55-ssh-key.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 type="ed25519"
-ssh_key_path="$HOME/.ssh/${type}"
+ssh_key_path="$HOME/.ssh/id_${type}"
 
 info "Creating SSH key..."
 [ -f "$ssh_key_path" ] && warn "SSH key found at ${ssh_key_path}, exiting." && return
--- .config/yadm/bootstrap.d/56-gh-login.sh
+++ .config/yadm/bootstrap.d/56-gh-login.sh
@@ -4,11 +4,6 @@ info "Logging you into github..."
 if gh auth status 1>/dev/null 2>&1; then
   warn "Already logged in, not logging in again."
 else
-  (yes | gh auth login -p ssh -h github.com -s admin:public_key)
+  gh auth login -p ssh -h github.com
+  info "Done."
 fi
-info "Adding SSH key..."
-if ! gh auth status | grep 'admin:public_key'; then
-  gh auth refresh -h github.com -s admin:public_key
-fi
-gh ssh-key add "${ssh_key_path}.pub"
-info "Done."
