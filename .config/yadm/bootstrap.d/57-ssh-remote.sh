915967ecdea11f2f4a56d4d854a56aa5b4ff50e9
LoricAndre <loric.andre@pm.me>
2023-01-29 18:11:27 +0100

    fix remote changing

--- .config/yadm/bootstrap.d/57-ssh-remote.sh
+++ .config/yadm/bootstrap.d/57-ssh-remote.sh
@@ -3,7 +3,7 @@
 cd "$HOME"
 info "Setting origin to SSH remote..."
 old_remote="$(yadm remote get-url origin)"
-new_remote="$(echo $old_remote | sed 's#https://#git@#;s#/#:#')"
+new_remote="$(echo $old_remote | sed 's#https://\([^/]\+\)/#git@\1:#')"
 
 yadm remote set-url origin "$new_remote"
 cd - >/dev/null || warn "Failed to get back to previous directory."
