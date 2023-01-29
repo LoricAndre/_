cc6b4329f04d4ad70fd19bd7925fc033171a1793
LoricAndre <loric.andre@pm.me>
2023-01-29 19:31:46 +0100

    reorder for env

--- .config/yadm/bootstrap.d/53-packages.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-l=($(cat "${packages}" "${packages}.extra"))
-info "Installing packages..."
-trizen -S --needed --noconfirm "${l[@]}"
-info "Done."
---
+++ .config/yadm/bootstrap.d/54-yadm-alt.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+info "Creating alt files..."
+zsh -i -c 'yadm alt'
+info "Done."
