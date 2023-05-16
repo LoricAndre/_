800ff716e843edc0587687ab42f0c97df999abb5
LoricAndre <loric.andre@pm.me>
2023-05-16 09:37:33 +0200

    rose pine

--- .config/zsh/colors.zsh
+++ .config/zsh/colors.zsh
@@ -1,18 +1,22 @@
-export color0=232634
+export color0=191724
 export color8=a5adce
-export color1=e78284 # RED
-export color9=d20f39
-export color2=a6d189 # GREEN
-export color10=40a02b
-export color3=e5c890 # YELLOW
-export color11=df8e1d
-export color4=7AA2F7 # BLUE
-export color12=1e66f5
-export color5=babbf1 # PINK
-export color13=7287fd
-export color6=81c8be # TEAL
-export color14=179299
-export color7=b5bfe2 # WHITE
-export color15=acb0be
+export color1=eb6f92 # RED
+export color9=ebbcba
+export color2=31748f # GREEN
+export color10=9ccf8d
+export color3=f6c177 # YELLOW
+export color11=ea9d34
+export color4=6e6a86 # BLUE
+export color12=908caa
+export color5=d7827e # PINK
+export color13=d7827e
+export color6=907aa9 # TEAL
+export color14=907aa9
+export color7=e0def4 # WHITE
+export color15=fffaf3
+
+export colorfg=$color7
+export colorbg=$color0
+
 export font='Iosevka Nerd Font Mono'
 export font_size=11
