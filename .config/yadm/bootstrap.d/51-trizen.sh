dbb7fceec9cc06abd26ea8877e4eab61de394b69
LoricAndre <loric.andre@pm.me>
2023-05-16 10:13:52 +0200

    add noconfirm

--- .config/yadm/bootstrap.d/51-trizen.sh
+++ .config/yadm/bootstrap.d/51-trizen.sh
@@ -3,16 +3,16 @@ info "Installing trizen..."
 trizen --version >/dev/null && warn "Trizen already installed, exiting." && return
 target="/tmp/trizen"
 info "Updating packages..."
-sudo pacman -Syu
+sudo pacman -Syu --needed --noconfirm 
 info "Done updating packages."
 info "Installing base-devel..."
-sudo pacman -S --needed base-devel
+sudo pacman -S --needed --noconfirm base-devel
 info "Done."
 info "Cloning to ${target}..."
 git clone https://aur.archlinux.org/trizen.git "${target}" || warn "Failed to clone repo to ${target}"
 info "Done."
 cd /tmp/trizen || err "Failed to cd to ${target}"
-makepkg -si
+makepkg -si --noconfirm --needed
 cd - >/dev/null || warn "Failed to get back to previous directory."
 info "Deleting ${target} dir..."
 rm -rf "${target}"
